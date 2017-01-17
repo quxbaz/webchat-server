7bf0036f9015129514454d4af82ce6541daaee53
quxbaz <quxbaz@gmail.com>
2017-01-17 14:06:05 -0500

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+app.use(express.static(__dirname + '/public'))
+
+// // views is directory for all template files
+// app.set('views', __dirname + '/views')
+// app.set('view engine', 'ejs')
+
+app.get('/', function(request, response) {
+  var result = 'hello world'
+  response.send(result)
+})
+
+app.listen(app.get('port'), function() {
+  console.log('App is running on port', app.get('port'))
+})
