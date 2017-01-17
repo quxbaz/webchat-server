af495de4fed7d1a39b46f5ae4c2efcd742d77466
quxbaz <quxbaz@gmail.com>
2017-01-17 14:38:06 -0500

    static file

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ app.use(express.static(__dirname + '/public'))
 // app.set('views', __dirname + '/views')
 // app.set('view engine', 'ejs')
 
-app.get('/foobar', function(request, response) {
-  var result = 'foobar'
-  response.send(result)
-})
+// app.get('/foobar', function(request, response) {
+//   var result = 'foobar'
+//   response.send(result)
+// })
 
 app.listen(app.get('port'), function() {
   console.log('App is running on port', app.get('port'))
